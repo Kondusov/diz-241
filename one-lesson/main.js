4432e9568f0ecbaca46d8443173a58822570c3e7
Учитель <teacher@TOP.ACADEMY>
2025-01-27 13:15:22 +0300

    form html

--- one-lesson/main.js
+++ one-lesson/main.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
     $(".owl-carousel").owlCarousel({
         items: 3,
         loop:true, //Зацикливаем слайдер
-        margin:25, //Отступ от картино если выводите больше 1
+        margin:10, //Отступ от картино если выводите больше 1
         nav:true, //Отключил навигацию
         autoplay:true, //Автозапуск слайдера
         smartSpeed:1000, //Время движения слайда
-        autoplayTimeout:2000, //Время смены слайда
+        autoplayTimeout:1000, //Время смены слайда
         responsive:{ //Адаптация в зависимости от разрешения экрана
             0:{
                 items:3
---
+++ one-lesson/send.php
@@ -0,0 +1,5 @@
+<?php
+//var_dump($_REQUEST);
+
+echo ('Имя' .$_REQUEST['name']);
+echo ('Возраст' .$_REQUEST['age']);
