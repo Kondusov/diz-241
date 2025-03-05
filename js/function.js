21c6be83732442883ae4af1dd6b7fd8d5cd5f88f
Учитель <teacher@TOP.ACADEMY>
2025-03-05 11:11:55 +0300

    Принимаем имя пользователя, и выводим его различными способами

--- js/function.js
+++ js/function.js
@@ -1,9 +1,16 @@
 
-function summ(a, b, c){
-    return a + b + c;
+function summ(a, h, c){
+    prompt('РАБОТАЕТ!')
+     
+    return a * h * c;
+    return 'Что-то вернули'
+    console.log('Этот участок кода работает');
+    
 }
 
-let result = summ(3,2,1.8)
-console.log(result)
-result = Math.round(result);
-console.log(result)
+console.log(summ(3, 4, 8))
+
+//let result = summ(3,2,1.8)
+//console.log(result)
+//let result2 = Math.round(result);
+//console.log(result2)
---
+++ js/promt.js
@@ -0,0 +1,4 @@
+const answer1 = prompt('Как тебя зовут?')
+alert('Поздравляем!!! Вас зовут' + answer1)
+
+document.body.innerHTML = 'Поздравляем!!! Вас зовут ' + answer1;
