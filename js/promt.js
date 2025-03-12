d1257de888810ad248d65c013acd2ec43494a050
Учитель <teacher@TOP.ACADEMY>
2025-03-12 11:17:40 +0300

    lesson if, else

--- js/promt.js
+++ js/promt.js
@@ -1,4 +1,13 @@
-const answer1 = prompt('Как тебя зовут?')
-alert('Поздравляем!!! Вас зовут' + answer1)
+const answer_promt = prompt('Как тебя зовут?');
 
-document.body.innerHTML = 'Поздравляем!!! Вас зовут ' + answer1;
+if(answer_promt != ""){
+    if(answer_promt.length > 2){
+        document.body.innerHTML = 'Поздравляем!!! Вас зовут ' + answer_promt;
+    }
+    else{
+        document.body.innerHTML = 'Имя ' + answer_promt + 'слишком короткое';
+    }
+}
+else{
+    document.body.innerHTML = 'Вы не ввели ваше имя';
+}
