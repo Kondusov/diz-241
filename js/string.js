952c087faa56893af31142711632260f4d4c0f16
Учитель <teacher@TOP.ACADEMY>
2025-03-19 11:44:19 +0300

    string methods

---
+++ js/string.js
@@ -0,0 +1,10 @@
+let string = 'Это строка';
+
+console.log(string);
+console.log(string.length);
+console.log(string[string.length-7]);
+console.log(string.indexOf(' '));
+console.log(string.slice(string.indexOf(' '),4));
+console.log(string.replace(' ',''));
+// string.slice(0,3);
+// string.slice(2);
